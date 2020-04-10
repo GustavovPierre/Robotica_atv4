b87693ca02118df702dc5aea9f5540f80c1ec8d9
Murilo Menezes <murilomenezes@Murilos-MacBook-Pro.local>
2020-04-09 21:30:12 -0300

    corrigindo cor.py

--- robotic4/scripts/cor.py
+++ robotic4/scripts/cor.py
@@ -26,7 +26,8 @@ cv_image = None
 media = []
 centro = []
 atraso = 1.5E9 # 1 segundo e meio. Em nanossegundos
-dist = None	
+dist = None
+detection_counter = 0
 
 area = 0.0 # Variavel com a area do maior contorno
 
@@ -80,27 +81,38 @@ if __name__=="__main__":
 	try:
 
 		while not rospy.is_shutdown():
-			vel = Twist()
+			vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
 			if len(media) != 0 and len(centro) != 0:
 				print("Média dos vermelhos: {0}, {1}".format(media[0], media[1]))
 				print("Centro dos vermelhos: {0}, {1}".format(centro[0], centro[1]))
 
 			
-				if dist > 0.3:
-
+				if detection_counter < 50:
 
 					if (media[0] > centro[0]):
 						
-						vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.1))
+						vel = Twist()
+						vel.angular.z = -0.1
 						
 
 					elif (media[0] < centro[0]):
 						
-						vel = Twist(Vector3(0,0,0), Vector3(0,0,0.1))
-					
-					else:
-						vel = Twist(Vector3(0.1,0,0), Vector3(0,0,0.1))
+						vel = Twist()
+						vel.angular.z = 0.1
 
+						detection_counter += 1
+					
+							
+
+
+				if dist > 0.3:
+
+					vel = Twist()
+					vel.linear.x = 0.1
+
+
+					
+					
 				else:
 					vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
 				
